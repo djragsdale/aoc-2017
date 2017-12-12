429a83390633a26aa480e56e39f73f52dd24f140
David Ragsdale <me@davidjragsdale.com>
2017-12-12 08:16:56 -0500

    Day 12 Part B

--- day12.js
+++ day12.js
@@ -3,33 +3,72 @@ log('Beginning program...')
 const input = require('./day12_input')
 	.split('\n')
 	.map((str) => str.split(/[\s,\<\>\-]+/)) // <-> ,
+	.map((arr) => arr.map((id) => parseInt(id, 10)))
 
 
 
 // Part A
-let inGroup = ['0']
-let newlyAdded = ['0']
-const maxRounds = 2000
-let roundCount = 0
-while (newlyAdded.length > 0 && roundCount < maxRounds) {
-	if (roundCount % 100 === 0) {
-		log(`entering round ${roundCount} with ${inGroup.length} matches`)
-	}
-	const _found = []
-	newlyAdded.forEach((id) => {
-		input
-			.filter((arr) => arr.filter((connection) => connection === id).length > 0)
-			.forEach((arr) => _found.push(arr[0]))
-	})
-	newlyAdded = [...new Set(_found.filter((id) => inGroup.indexOf(id) < 0))]
-	inGroup = [...inGroup, ...newlyAdded]
-	roundCount++
-	// log('added id', newlyAdded)
-}
-const result = `${inGroup.length} in ${roundCount} rounds`
-
+// let inGroup = [0]
+// let newlyAdded = [0]
+// const maxRounds = 2000
+// let roundCount = 0
+// while (newlyAdded.length > 0 && roundCount < maxRounds) {
+// 	if (roundCount % 100 === 0) {
+// 		log(`entering round ${roundCount} with ${inGroup.length} matches`)
+// 	}
+// 	const _found = []
+// 	newlyAdded.forEach((id) => {
+// 		input
+// 			.filter((arr) => arr.filter((connection) => connection === id).length > 0)
+// 			.forEach((arr) => _found.push(arr[0]))
+// 	})
+// 	newlyAdded = [...new Set(_found.filter((id) => inGroup.indexOf(id) < 0))]
+// 	inGroup = [...inGroup, ...newlyAdded]
+// 	roundCount++
+// }
+// const result = `${inGroup.length} in ${roundCount} rounds`
 
 // Part B
+let groups = []
+let nextId = 0
+const maxGroups = 2000
+let groupCount = 0
+while (nextId <= input[input.length - 1][0] && groupCount < maxGroups) {
+	if (groupCount % 100 === 0) {
+		log(`entering round ${groupCount} at id ${nextId}`)
+	}
+	let inGroup = [nextId]
+	let newlyAdded = [nextId]
+	const maxRounds = 2000
+	let roundCount = 0
+	while (newlyAdded.length > 0 && roundCount < maxRounds) {
+		// if (roundCount % 100 === 0) {
+		// 	log(`entering round ${roundCount} with ${inGroup.length} matches`)
+		// }
+		const _found = []
+		newlyAdded.forEach((id) => {
+			input
+				.filter((arr) => arr.filter((connection) => connection === id).length > 0)
+				.forEach((arr) => _found.push(arr[0]))
+		})
+		newlyAdded = [...new Set(_found.filter((id) => inGroup.indexOf(id) < 0))]
+		inGroup = [...inGroup, ...newlyAdded]
+		roundCount++
+	}
+	groups.push(inGroup)
+	for (let i = 0; i < input.length + 100; i++) {
+		const isInAGroup = groups
+			.map((group) => group
+					.reduce((acc, next) => acc || (next === i), false))
+			.reduce((acc, next) => acc || next, false)
+		if (!isInAGroup) {
+			nextId = i
+			break
+		}
+	}
+	groupCount++
+}
+result = groups.length
 
 
 
