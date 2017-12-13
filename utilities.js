fb08a88bdfbab572cd632c0f43361d51a20778d0
David Ragsdale <me@davidjragsdale.com>
2017-12-13 07:39:59 -0500

    Day 13 Part A

--- utilities.js
+++ utilities.js
@@ -6,6 +6,8 @@ const addWrap = (base, toAdd, breakAt) => {
 	}
 }
 
+const log = (...params) => console.log(`${new Date().toTimeString()} >>>`, ...params)
+
 const range = (min, max, step = 1) => {
 	let _max = max
 	let _min = min
@@ -27,7 +29,7 @@ module.exports = {
 	isNull: (val) => typeof val === 'null',
 	isNullOrUndefined: (val) => typeof val === 'null' || typeof val === 'undefined',
 	isUndefined: (val) => typeof val === 'undefined',
-	log: (...params) => console.log(`${new Date().toTimeString()} >>>`, ...params),
+	log,
 	range,
 	sum: (val0, val1) => val0 + val1,
 	sumStr: (val0, val1) => ~~val0 + ~~val1,
